aea90d9876a71e30429ae92bfd9b193a1e820c6b
Tottadi sai sasank <76509958+sasank2323@users.noreply.github.com>
2025-05-11 20:37:12 +0530

    Update DestinationData.java

--- src/main/java/com/example/demo/Controller/TravelDestinationList/DestinationData.java
+++ src/main/java/com/example/demo/Controller/TravelDestinationList/DestinationData.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 @RestController
-@RequestMapping("/destinationdeatils")
+@RequestMapping("/CICDCHECKING")
 public class DestinationData {
 
     @Autowired
